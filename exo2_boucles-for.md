f3312091aa00248ce44d48b7b04d13a2f746ffc8
DawiT <Dawid@Tararuj.be>
2021-01-07 16:38:56 +0100

    fin

---
+++ exo2_boucles-for.md
@@ -0,0 +1,8 @@
+- # Exo 1 Boucle FOREACH
+    - ## Créez un un array qui contient 5 éléments
+    - ## Affichez touts les éléments a l'aide d'une boucle foreach
+    
+-  # Exo 2 Boucles FOREACH
+    ## - Creer une varaible de type array et inserez toutes les personnes de la classe
+    ## - A l'aide d'une boucle foreach, dites bonjour + prenom à chaque personne du tableau
+    ## - Afiichez egalement sa position
---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+let fruits = [
+    'bananes',
+    'pommes',
+    'cerises',
+    'oranges',
+    'tomates'
+]; 
+
+
+fruits.forEach(function(e, i){
+    console.log(`fruits = ${e} `);
+});
+
+
+let coding16 = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "maxence", "zulma"]; 
+
+coding16.forEach((element, index) => {
+    console.log(`numero: ${index}, Bonjour ${element}`);
+});
